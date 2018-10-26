65adf4b4da276a51a9060f752b0c92bbfed2cdc4
kolldavi <kolldavi@gmail.com>
2018-10-26 10:29:53 -0500

    add proptypes

--- app/index.js
+++ app/index.js
@@ -5,12 +5,21 @@ require('./index.css');
 
 class Badge extends React.Component {
 	render() {
-		return <div>Hello World!</div>;
+		return (
+			<div>
+				<img src={this.props.img} alt="avatar" style={{ width: 100, height: 100 }} />
+				<h1>Name: {this.props.name}</h1>
+				<h3>Username: {this.props.username}</h3>
+			</div>
+		);
 	}
 }
-Badge.PropTypes = {
+Badge.propTypes = {
 	img: PropTypes.string.isRequired,
 	name: PropTypes.string.isRequired,
 	username: PropTypes.string.isRequired
 };
-ReactDOM.render(<Badge />, document.getElementById('app'));
+ReactDOM.render(
+	<Badge img="https://avatars3.githubusercontent.com/u/7830118?s=400&v=4" name="David Koller" username="kolldavi" />,
+	document.getElementById('app')
+);
